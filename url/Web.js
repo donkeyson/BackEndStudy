03f2d8e7c73d31c7a48380321380f4e9ace2bd63
donkeyson <88279704+donkeyson@users.noreply.github.com>
2022-01-15 22:53:34 +0900

    Update Web.js

--- url/Web.js
+++ url/Web.js
@@ -1,15 +1,18 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
+
 var app = http.createServer(function(request, response){
-  var url = request.url;
-  if(url == '/') {
-    url = '/index.html';
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query; //{ ID: 'HTML' }
+  if(_url == '/') {
+    _url = '/index.html';
   }
-  if(url == '/favicon.ico'){
+  if(_url == '/favicon.ico'){
     return response.writeHead(404);
   }
   response.writeHead(200);
-  response.end(fs.readFileSync{_dirname + url}));
+  response.end(queryData.id); //HTML
 
 });
 app.listen(3000);
