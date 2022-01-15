95c6c85a21fae6fe397993481ac1c5da21b801f7
donkeyson <88279704+donkeyson@users.noreply.github.com>
2022-01-15 22:45:35 +0900

    Update Web.js

--- url/Web.js
+++ url/Web.js
@@ -2,7 +2,7 @@ var http = require('http');
 var fs = require('fs');
 var app = http.createServer(function(request, response){
   var url = request.url;
-  if(request.url == '/') {
+  if(url == '/') {
     url = '/index.html';
   }
   if(request.url == '/favicon.ico'){
